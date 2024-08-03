ba922602fa85f6f739944b06085227979f35e9aa
Josh McCulloch <joshmcculloch@gmail.com>
2024-08-03 09:09:14 -0700

    add engagement detection

--- src/detection.ts
+++ src/detection.ts
@@ -1,4 +1,5 @@
 import { NotificationData, notifications } from '@mantine/notifications';
+import { isBlacklisted, profileIdFromUrl } from './common';
 
 const CLONED_NICKNAME_REGEX = /^https:\/\/www.facebook.com\/.*\d+.*\D+\/$/;
 const PRONOUN_REGEX = /updated (.*) profile picture/;
@@ -10,6 +11,8 @@ export function detection() {
     checkForClonedNickname();
     checkForAngryReactions();
     checkForBackdatedPosts();
+
+    engagement();
 }
 
 function checkForClonedNickname(): void {
@@ -61,6 +64,76 @@ function checkForBackdatedPosts(): void {
 
         return true;
     });
+}
+
+function engagement(): void {
+    const profileName = getProfileName();
+
+    const reactionSet = new Set();
+
+    let selfReaction = false;
+    let blacklistedReaction = false;
+
+    Array.from(document.querySelectorAll('div[data-visualcompletion="ignore-dynamic"] span[aria-label="See who reacted to this"]')).some((e: Element) => {
+
+        if(selfReaction) {
+            return true;
+        }
+
+        // @ts-ignore
+        const reactionButton = e.nextSibling?.querySelector('div[role="button"]');
+
+        if(reactionButton) {
+            reactionButton.click();
+        }
+
+        // TODO: This needs to be ported to a mutation observer and get rid of setTimeout
+        setTimeout(() => {
+            const reactions = document.querySelectorAll('div[data-visualcompletion="ignore-dynamic"] span[dir="auto"] a[role="link"]');
+            reactions.forEach((e) => {
+                reactionSet.add(e.textContent);
+
+                if(blacklistedReaction) {
+                    return;
+                }
+
+                const profileLink = e as HTMLAnchorElement;
+
+                const profileId = profileIdFromUrl(profileLink.href);
+                if(profileId) {
+                    isBlacklisted(profileId, profileLink, () => {
+                        showNotification({
+                            title: 'Blacklisted Engagement',
+                            message: '🤬 Blacklisted Profile Engagement Detected',
+                            color: 'red'
+                        });
+
+                        blacklistedReaction = true;
+                    });
+                }
+            });
+
+            // @ts-ignore
+            const close = document.querySelector('div[aria-label="Close"]')?.click();
+
+            if(reactionSet.has(profileName)) {
+                if(!selfReaction) {
+                    showNotification({
+                        title: 'Self Engagement',
+                        message: '🤡 Self Engagement Detected',
+                        color: 'red'
+                    });
+                }
+
+                selfReaction = true;
+            }
+        }, 500);
+    });
+}
+
+function getProfileName(): string {
+    // TODO: null safety
+    return document.querySelector('div[data-visualcompletion="ignore-dynamic"] a[role="link"]')!.textContent!
 }
 
 function showNotification(notification: NotificationData): void {
