206d32529b813ab113b742d7eed6048c7d497238
Josh McCulloch <joshmcculloch@gmail.com>
2024-08-20 09:54:58 -0700

    #50 - quiet down react warnings

--- src/reports_modal.tsx
+++ src/reports_modal.tsx
@@ -7,8 +7,12 @@ import { emojiForReportType } from './common';
  *
  */
 export function ReportsModal(props: ReportsModalProps) {
-    const rows = props.reports.map((report) => (
-        <Table.Tr>
+    // NOTE: key={index} "anti-pattern" to quiet down react warnings
+    // We aren't re-rendering this table so it should be sufficient.
+    // See: https://stackoverflow.com/questions/28329382/understanding-unique-keys-for-array-children-in-react-js
+    // TODO: Switch to DB ID if exposed via Report type
+    const rows = props.reports.map((report, index) => (
+        <Table.Tr key={index}>
             <Table.Td>{report.createdAt}</Table.Td>
             <Table.Td>{report.dispute ? '👎' : '👍'}</Table.Td>
             <Table.Td>{emojiForReportType(report.type)} {report.type}</Table.Td>
